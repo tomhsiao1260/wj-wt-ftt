cbf27375e6bd721920e5b4abb214f7095b2d7806
Hsiao Yao <tomtogo1260@gmail.com>
2024-08-29 15:14:13 +0800

    add pycache into gitignore, plz work ...

--- src/main/servers/pythonAPIServer/handle_nrrd.py
+++ src/main/servers/pythonAPIServer/handle_nrrd.py
@@ -1,7 +1,7 @@
 import os
-import nrrd
-import tifffile
-import numpy as np
+# import nrrd
+# import tifffile
+# import numpy as np
 # import open3d as o3d
 
 from __main__ import app
@@ -14,18 +14,18 @@ def handle_nrrd():
     buffer = request.data
     data_length = len(buffer)
 
-    # x, y, z
-    data = np.frombuffer(buffer, dtype=np.uint8)
-    data = data.reshape(256, 256, 256)
-    # data = data.reshape(size, size, size)
-    data = np.where(data == 2, 2, 0).astype(np.uint8)
+    # # x, y, z
+    # data = np.frombuffer(buffer, dtype=np.uint8)
+    # data = data.reshape(256, 256, 256)
+    # # data = data.reshape(size, size, size)
+    # data = np.where(data == 2, 2, 0).astype(np.uint8)
 
-    data1 = np.where(data == 2, 255, 0).astype(np.uint8)
+    # data1 = np.where(data == 2, 255, 0).astype(np.uint8)
 
-    # z, y, x
-    tifffile.imwrite('../../../../../mask-to-mesh-test/01744_02256_02768/output.tif', data1.transpose(2, 1, 0))
-    nrrd.write('../../../../../mask-to-mesh-test/01744_02256_02768/output.nrrd', data.transpose(2, 1, 0))
-    # tifffile.imwrite('./output/output.tif', data.transpose(2, 1, 0))
+    # # z, y, x
+    # tifffile.imwrite('../../../../../mask-to-mesh-test/01744_02256_02768/output.tif', data1.transpose(2, 1, 0))
+    # nrrd.write('../../../../../mask-to-mesh-test/01744_02256_02768/output.nrrd', data.transpose(2, 1, 0))
+    # # tifffile.imwrite('./output/output.tif', data.transpose(2, 1, 0))
 
     # length
     return jsonify({"status": "success", "data_length": data_length})
